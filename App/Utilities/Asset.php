e1c5ecb601b23b22bb0d9a577033a7db77af93e5
saba <sereno.project@gmail.com>
2021-03-26 12:05:13 +0430

    improve utility method calls

--- App/Utilities/Asset.php
+++ App/Utilities/Asset.php
@@ -1,36 +1,32 @@
 <?php
 namespace App\Utilities;
 class Asset{
-   public static function get_base_url(string $route){
-       return $_ENV['BASE_URL']."assets".$route;
-   }
-
-    public static function get_css_url(string $route){
-        return $_ENV['BASE_URL']."assets/css".$route;
-    }
-
-    public static function get_image_url(string $route){
-        return $_ENV['BASE_URL']."assets/img".$route;
-    }
-
-    public static function get_js_url(string $route){
-        return $_ENV['BASE_URL']."assets/js".$route;
-    }
-
-    public static function get_fonts_url(string $route){
-        return $_ENV['BASE_URL']."assets/fonts".$route;
-    }
-
-//    public static function __callStatic($name, $arguments)
-//    {
+//   public static function get_base_url(string $route){
+//       return $_ENV['BASE_URL']."assets".$route;
+//   }
 //
-//
-//        if($name != "get"){
-//            return $_ENV['HOST'] . "assets/{$name}/" . $arguments[0];
-//        }
-//
-//        return $_ENV['HOST'] . "assets/" . $arguments[0];
-//
+//    public static function get_css_url(string $route){
+//        return $_ENV['BASE_URL']."assets/css".$route;
 //    }
+//
+//    public static function get_image_url(string $route){
+//        return $_ENV['BASE_URL']."assets/img".$route;
+//    }
+//
+//    public static function get_js_url(string $route){
+//        return $_ENV['BASE_URL']."assets/js".$route;
+//    }
+//
+//    public static function get_fonts_url(string $route){
+//        return $_ENV['BASE_URL']."assets/fonts".$route;
+//    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        if($name != "get_base_url"){
+            return $_ENV['BASE_URL']. "assets/" . $arguments[0];
+        }
+        return $_ENV['BASE_URL']. "assets/";
+    }
 
 }
--- index.php
+++ index.php
@@ -5,7 +5,9 @@ use App\Utilities\Url;
 
 include __DIR__."/bootstrap/init.php";
 
-//echo Asset::get_base_url("/css/style.css");
+echo Asset::get_base_url()."</br>";
+echo Asset::get_css_url("css/style.css")."</br>";
+echo Asset::get_js_url("js/js.js");
 
-echo Url::current_url();
+//echo Url::current_url();
 

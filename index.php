a530a187b9df095afccd906ba6be6700fedb2068
saba <sereno.project@gmail.com>
2021-03-26 20:28:15 +0430

    another implementation of routes using class and associative array

--- index.php
+++ index.php
@@ -1,21 +1,8 @@
 <?php
 #front controller
-use App\Utilities\Asset;
-use App\Utilities\Url;
+use App\Core\StupidRoutes;
 
 include __DIR__."/bootstrap/init.php";
 
-//echo Asset::get_base_url()."</br>";
-//echo Asset::get_css_url("css/style.css")."</br>";
-//echo Asset::get_js_url("js/js.js");
-//echo Url::current_url();
-
-#the simplest and most stupid way to develop routers
-
-$route= Url::current_route();
-if ($route=='/bg/blue')
-    include BASEPATH.'views/colors/blue.php';
-if ($route=='/bg/red')
-    include BASEPATH.'views/colors/red.php';
-if ($route=='/bg/green')
-    include BASEPATH.'views/colors/green.php';
+$router=new StupidRoutes();
+$router->run();

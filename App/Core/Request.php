0bd02c15aae3b2fff6188fa55a255ac7ac881e2b
saba <sereno.project@gmail.com>
2021-03-27 14:35:47 +0430

    request implementation

--- App/Core/Request.php
+++ App/Core/Request.php
@@ -1,9 +1,71 @@
 <?php
 namespace App\Core;
 
+
 class Request{
+    private $params;
+    private $agent;
+    private $ip;
+    private $method;
+    private $queryString;
+
     public function __construct()
     {
-        echo "construct";
+        $this->params=$_REQUEST;
+        $this->agent=$_SERVER['HTTP_USER_AGENT'];
+        $this->ip=$_SERVER['REMOTE_ADDR'];
+        $this->method=$_SERVER['REQUEST_METHOD'];
+        $this->queryString=$_SERVER['QUERY_STRING'];
+
     }
+
+
+    public function getMethod(): mixed
+    {
+        return $this->method;
+    }
+
+    public function getAgent(): mixed
+    {
+        return $this->agent;
+    }
+
+    public function getIp(): mixed
+    {
+        return $this->ip;
+    }
+
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+
+    public function input($key)
+    {
+        return $this->params[$key] ?? null;
+    }
+
+    public function isset($key)
+    {
+        return isset($this->params[$key]);
+    }
+
+//    public function redirect($route)
+//    {
+//        header("location:");
+//        die();
+//    }
+
+    public function getQueryString(): mixed
+    {
+        return $this->queryString;
+    }
+
+    public function __get(string $param)
+    {
+        if (array_key_exists($param, $this->params)) {
+            return $this->params[$param];
+        }
+    }
+
 }
--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
 #front controller
-use App\Core\StupidRoutes;
+use App\Core\Request;
 
 include __DIR__."/bootstrap/init.php";
 
-$router=new StupidRoutes();
-$router->run();
+$request=new Request();
+//var_dump($request);
+//echo $request->getIp();
+//var_dump( $request->getParams());
+//var_dump( $request->input('name'));
+//var_dump($request->isset('name'));
+//var_dump($request->isset('age'));
+
+//
+//$query=$request->getQueryString();
+//$parts = parse_url($query);
+//parse_str($parts['query'], $query);
+//echo $query['name'];
+
+
+
+var_dump($request->edu);
